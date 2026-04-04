1aab044e00aa175c5a00c6457bf3d8e235a541af
Yun-Tse Tsai <yuntsebaryon@gmail.com>
2026-04-03 18:39:13 -0700

    update the parameters

--- MCProd/g4/runDumpTree.py
+++ MCProd/g4/runDumpTree.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 import argparse
-import dumpTree
+import dumpTreeNew
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser( description = 'Process GEANT4 simulation using edep-sim.' )
 
     parser.add_argument('--sample', type = str, required = True,
-                        choices = ['signal', 'cosmic', 'BRN', 'dirt', 'HOG'],
-                        help = 'Sample types: signal, dirt, cosmic, BRN, HOG')
+                        choices = ['signal', 'cosmic', 'BRN', 'outside', 'HOG'],
+                        help = 'Sample types: signal, outside, cosmic, BRN, HOG')
     parser.add_argument('--dir', type = str, required = True, help = 'directory of input and output files')
 
     args = parser.parse_args()
@@ -16,8 +16,8 @@ if __name__ == "__main__":
     sample = args.sample
     sampleConfig = { 'signal': {'label': 'NueArCC', 'prefix': 'nueArCC_sns', 'nFiles': 10, 'nEventsPerFile': 10000 },
                      'cosmic': {'label': 'Cosmics', 'prefix': 'CosmicFlux', 'nFiles': 200, 'nEventsPerFile': 100000 },
-                     'BRN': { 'label': 'BRN', 'prefix': 'BRN', 'nFiles': 50, 'nEventsPerFile': 10000 },
-                     'dirt': { 'label': 'NueArCCdirt', 'prefix': 'nueArCC_sns', 'nFiles': 40, 'nEventsPerFile': 10000 },
+                     'BRN': { 'label': 'BRN', 'prefix': 'BRN', 'nFiles': 80, 'nEventsPerFile': 10000 },
+                     'outside': { 'label': 'NueArCCoutside', 'prefix': 'nueArCC_sns', 'nFiles': 40, 'nEventsPerFile': 10000 },
                      'HOG': { 'label': 'HOG', 'prefix': 'HOG', 'nFiles': 1, 'nEventsPerFile': 1 } }
     
     for iFile in range( sampleConfig[sample]['nFiles'] ):
@@ -25,4 +25,4 @@ if __name__ == "__main__":
         h5File = f'{args.dir}/g4/{sampleConfig[sample]['label']}/{sampleConfig[sample]['prefix']}_g4_{iFile:04d}.h5'
 
         print(f'Dumping {rootFile}...')
-        dumpTree.dump(rootFile, h5File, True)
+        dumpTreeNew.dump(rootFile, h5File, True)
--- MCProd/g4/runDumpTreeParallel.py
+++ MCProd/g4/runDumpTreeParallel.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import dumpTree
+import dumpTreeNew
 
 from concurrent.futures import ProcessPoolExecutor
 
@@ -9,22 +9,22 @@ def runDumpTree(iBatch):
     sampleDir = '/Users/yuntse/data/coherent/SNeNDSens'
     label = 'Cosmics'
     prefix = 'CosmicFlux'
-    nFiles = 200
+    nFiles = 100
 
     for iFile in range( nFiles ):
-        jFile = (iBatch+1)*nFiles + iFile
-        rootFile = f'{sampleDir}/g4/{label}/{(iBatch+1)*nFiles:04d}/{prefix}_g4_{jFile:04d}.root'
-        h5File = f'{sampleDir}/g4/{label}/{(iBatch+1)*nFiles:04d}/{prefix}_g4_{jFile:04d}.h5'
+        jFile = iBatch*nFiles + iFile
+        rootFile = f'{sampleDir}/g4/{label}/{iBatch*nFiles:04d}/{prefix}_g4_{jFile:04d}.root'
+        h5File = f'{sampleDir}/g4/{label}/{iBatch*nFiles:04d}/{prefix}_g4_{jFile:04d}.h5'
 
         print(f'Dumping {rootFile}...')
-        dumpTree.dump(rootFile, h5File, True)
+        dumpTreeNew.dump(rootFile, h5File, True)
 
     return
 # def runDumpTree()
 
 if __name__ == "__main__":
 
-    nBatches = [ 0, 1, 2, 3, 4 ]
+    nBatches = list( range( 12) )
 
     # Run in parallel
     with ProcessPoolExecutor() as executor:
